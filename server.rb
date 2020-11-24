a731d71d11df6fe6dd9414c305cc0588b03ac74e
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-11-23 17:01:13 -0800

    Initial commit!

---
+++ server.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'aws-sdk-core'
+
+$role_credentials = Aws::AssumeRoleCredentials.new(
+  role_arn: ENV['ROLE_ARN'],
+  role_session_name: "aws-credentials-server"
+)
+
+configure do
+  set :bind, "0.0.0.0"
+  set :port, "80"
+end
+
+get '/' do
+  'aws-credentials-server'
+end
+
+get '/latest/meta-data/iam/security-credentials' do
+  'local-credentials'
+end
+
+get '/latest/meta-data/iam/security-credentials/local-credentials' do
+  content_type :json
+  credentials
+end
+
+get '/latest/meta-data/instance-id' do
+  'aws-credentials-server'
+end
+
+get '/latest/meta-data/iam/info' do
+  '{"Code": "Success"}'
+end
+
+get '/latest/meta-data/dynamic/instance-identity/document' do
+  '{"region": "us-east-1"}'
+end
+
+def credentials
+  creds = $role_credentials.credentials
+  expiry = $role_credentials.expiration
+
+  {
+    Code: "Success",
+    LastUpdated: Time.now.utc.iso8601,
+    Type: "AWS-HMAC",
+    AccessKeyId: creds.access_key_id,
+    SecretAccessKey: creds.secret_access_key,
+    Token: creds.session_token,
+    Expiration: expiry.iso8601
+  }.to_json
+end
